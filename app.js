a4860ae3b5a995767c44c23c208b418113e1c6e2
STUDENT <hmhsabbasiaptech07@gmail.com>
2025-10-17 22:55:55 -0700

    allfiles

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+
+    $("p").html("<i>hello<i/>")
+    $(".para").html("<i>paragraph<i/>")
+    $("#ir").html("<i>hasnain<i/>")
+    $(".heading").css('color' , 'red')
+})
